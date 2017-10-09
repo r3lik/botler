55d30ddfff1d449cb3f1e0dd0e2710b12d03080f
Mike Czarny <me@r3lik.com>
2017-10-09 04:48:15 +0000

    Add working botler script and api key stored as env var

--- botler.py
+++ botler.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python3.5
 import discord
 import asyncio
+import os
 
 client = discord.Client()
+#uses environment variable set in .bash_profile
+secret = os.environ['DISCORD_TOKEN']
 
 @client.event
 async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
 
 @client.event
 async def on_message(message):
-	if message.content.startswith('!ping'):
-		await client.send_message(message.channel, 'Pong!')
+        if message.content.startswith('!ping'):
+                await client.send_message(message.channel, 'Pong!')
 
-client.run('xxxx')
+client.run(secret)
